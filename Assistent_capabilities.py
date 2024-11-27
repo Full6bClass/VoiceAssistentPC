ae9669bed8ee094fe7e9ec3ffe1504b6f65a1d0e
Full6bClass <162415061+Full6bClass@users.noreply.github.com>
2024-11-27 22:17:28 +0300

    key update

--- Assistent_capabilities.py
+++ Assistent_capabilities.py
@@ -80,6 +80,31 @@ class Action_controll:
         """
         return {
                 'стоп': 'stop',
+                'остановись': 'stop',
+                'звук': {
+                    # Звук тише
+                    ('убавить', 'уменьшить', 'снизь', 'приубавь', 'приглуши', 'тише'): [pc_action.volume_down, 'INTEGER', 0],
+
+                    # Звук громче
+                    ('прибавить', 'увеличить', 'повысить', 'усилить', 'поднять'): 'voice_high',
+
+                        }
+                }
+
+    def action_list_v2(self):
+        """
+        На данный момент структура хранимых функций:
+        [Функция, тип передаваемых данных, единаразовое действие или долгосрочная функция]
+        1. передается экземпляр функции
+        2. Тип данных если 0 то входные данные не требуются
+        3. Если функция вроде включить/выключить звук не требующая дальнейшего контроля то 0
+        если функция вроде браузера и будет работать в потоке то возвращаеться имя ключа в пуле потоков
+
+        :return:
+        """
+        return {
+                'стоп': 'stop',
+                'остановись': 'stop',
                 'звук': {
                     # Звук тише
                     'убавить': [pc_action.volume_down, 'INTEGER', 0],
@@ -97,6 +122,7 @@ class Action_controll:
 
                         }
                 }
+
     def text_stable(self, engin):
         """
         Еще не реализована функция сохранения экземпляров класса для тех кто этого требует
@@ -190,6 +216,82 @@ class Action_controll:
         # Нужно или обрабатывать тут функции и запоминать потоки или вернуть обратно для записи их существование
         # Если исполняемые на моменте то нечего
 
+    def search_fuction_v2(self, text, enter_token):
+        '''
+
+        :param text:
+        :param enter_token:
+        :return:
+        '''
+        print('search_fuction=', text, enter_token)
+        # Разбираем текст на токены
+        tokens = text.split()[enter_token:]
+        text = ' '.join(tokens)
+        # Это у нас список действий
+        all_action_list = self.action_list_v2()
+        searche_actions = {'action': [], 'chain': []}
+        # Ищем токены в предложении
+        for _ in range(0, len(tokens)):
+            # Для сбора токенов по которым найдено
+            parent_chain = []
+
+            # Ищет функции
+            def token_search(action_list):
+                rait_engin = {'key': str(), 'rait': 0}
+
+                # Перебираем сравнение токенов с ключами
+                for key_input in action_list.keys():
+                    if type(key_input) == tuple:
+                        for key in key_input:
+                            for token in tokens:
+                                rait = fuzz.ratio(key, token)
+                                # Находим лучший рейтинг
+                                if rait > 70 and rait > rait_engin['rait']:
+                                    rait_engin['key'], rait_engin['rait'] = key, rait
+                    else:
+                        key = key_input
+                        for token in tokens:
+                            rait = fuzz.ratio(key, token)
+                            # Находим лучший рейтинг
+                            if rait > 70 and rait > rait_engin['rait']:
+                                rait_engin['key'], rait_engin['rait'] = key, rait
+
+                # Если нашли нужные ключи
+                if rait_engin['rait'] != 0:
+                    # Вытаскиваем индекс токена подошедшего
+                    index = tokens.index(rait_engin['key'])
+                    # Индекс заменятьеся на 0 что бы сохранить порядок но убрать повторение значения
+                    tokens[index] = '0'
+                    # В найденные пишем индекс в строке токена
+                    parent_chain.append(index)
+                    # Записываем функцию токена
+                    result = action_list[rait_engin['key']]
+                    # Если результат это словарь то ищем в нем
+                    if type(result) == dict:
+                        return token_search(result)
+                    else:
+                        return {'action': result, 'chain': parent_chain}
+                else:
+                    return False
+
+            # Получаем найденную функцию и её создателей
+            result = token_search(action_list=all_action_list)
+            # Если функция найдена
+            if result is not False:
+                # Если функция строка отправляем запроснику для обработки
+                if type(result['action']) == str:
+                    yield {**result, 'text': text}
+                else:
+                    # Если функция это какая то реальная функция то запускаем её работу
+                    self.action_start({**result, 'text': text})
+                    yield True
+                # searche_actions['action'].append(result)
+                # searche_actions['chain'].append(parent_chain)
+            else:
+                break
+
+        # Нужно или обрабатывать тут функции и запоминать потоки или вернуть обратно для записи их существование
+        # Если исполняемые на моменте то нечего
 
 
 if __name__ == '__main__':
